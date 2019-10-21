9f9179ed0f874479f1987cc77860b29b09b34a1b
Richard <fahitch+github@gmail.com>
2019-10-21 23:10:40 +0100

    adds add book by title function

--- library_class.rb
+++ library_class.rb
@@ -1,10 +1,11 @@
 class Library
 
+attr_accessor :books_array
+
 #create a class for a library that has an array of books
 
-
-def initialize (books_array)
-  @books_array = books_array
+def initialize (books_list)
+  @books_list = books_list
 end
 
   # def initialize (title, rental_details)
@@ -16,12 +17,12 @@ end
 #create a getter for the books
 
   def get_book(book_num)
-    return @books_array[book_num]
+    return @books_list[book_num]
   end
 
   def find_by_title(query_title)
     match = nil
-    for book in @books_array
+    for book in @books_list
       match = book if book[:title] == query_title
     end
     return match
@@ -30,6 +31,13 @@ end
   def find_rental_details_by_title(book_title)
     book_found = find_by_title(book_title)
     return book_found[:rental_details]
+  end
+
+  def add_book_by_title(new_title)
+  # new_student_name = "" if new_student_name.empty
+  # new_date = "" if new_student_date.empty
+    @books_list.push(
+      {title: new_title, rental_details: {student_name: "", date:""}})
   end
 
 end
--- specs/library_class_spec.rb
+++ specs/library_class_spec.rb
@@ -33,48 +33,70 @@ def setup
 end
 
 
-def test_get_book
+  def test_get_book
 
-  library_class = Library.new(@books_list)
-  result = library_class.get_book(1)
-  assert_equal({ title: "the_pearl",
-    rental_details: {
-      student_name: "Marc",
-      date: "20/11/20"
-    }
+    library_class = Library.new(@books_list)
+    result = library_class.get_book(1)
+    assert_equal({ title: "the_pearl",
+      rental_details: {
+        student_name: "Marc",
+        date: "20/11/20"
+      }
 
-  }, result)
-end
+      }, result)
+  end
 
 # Create a method that takes in a book title
 # and returns all of the information about that book.
 
-def test_get_book_from_title
-  library_class=Library.new(@books_list)
-  result = library_class.find_by_title("the_pearl")
-  assert_equal(
-    { title: "the_pearl",
-    rental_details: {
-      student_name: "Marc",
-      date: "20/11/20"
-    }
-    }, result)
-end
+  def test_get_book_from_title
+    library_class=Library.new(@books_list)
+    result = library_class.find_by_title("the_pearl")
+    assert_equal(
+      { title: "the_pearl",
+        rental_details: {
+          student_name: "Marc",
+          date: "20/11/20"
+        }
+      }, result)
+  end
 
 # Create a method that takes in a book title
 # and returns only the rental details for that book.
 
-def test_return_rental_details_by_title__0
-  library_class = Library.new(@books_list)
-  result = library_class.find_rental_details_by_title("war_and_peace")
-  assert_equal({
+  def test_return_rental_details_by_title__0
+    library_class = Library.new(@books_list)
+    result = library_class.find_rental_details_by_title("war_and_peace")
+    assert_equal({
     student_name: "Yana",
     date: "10/11/20"
-  }, result)
+    }, result)
   end
 
+# tests for a method that
+# that takes in a book title and adds it to our book list
+# (add a new hash for the book with
+# the student name and date being left as empty strings)"
 
+def test_add_book_by_title
+  library_class = Library.new(@books_list)
 
+#creates variable to save on typing
+  new_title = "around_the_world_in_80_days"
+
+#calls function to add book by title
+  library_class.add_book_by_title(new_title)
+
+#looks up book with this new title for result check
+  result = library_class.find_by_title(new_title)
+
+  assert_equal({ title: "around_the_world_in_80_days",
+  rental_details: {
+    student_name: "",
+    date: ""
+  }
+  }, result)
+end
 
 
 
